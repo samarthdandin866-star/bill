735c83eb58ac17d59764a3bf6423641659b98ff7
Samarth <samarthdandin866@gmail.com>
2025-10-04 11:30:27 +0530

    save

--- bill.py
+++ bill.py
@@ -11,3 +11,21 @@ bill = units * rate
 
 # Display result
 print(f"Electricity Bill = ₹{bill:.2f}")
+
+# Program to calculate final amount after discount
+
+# Take input from user
+amount = float(input("Enter the total purchase amount: "))
+
+# Check if discount is applicable
+if amount > 1000:
+    discount = amount * 0.10   # 10% discount
+    final_amount = amount - discount
+    print(f"Discount of ₹{discount:.2f} applied!")
+else:
+    discount = 0
+    final_amount = amount
+    print("No discount applied.")
+
+# Display final bill
+print(f"Final Amount to Pay: ₹{final_amount:.2f}")
